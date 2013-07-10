e6d718fbc9f265d3a973a7d109958aead4a8fc5d
John Smith <algo@algo.com>
2013-07-10 19:22:04 +0000

    now reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var readBuff = fs.readFileSync('index.html');
+
+var theText = readBuff.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(theText);
 });
 
 var port = process.env.PORT || 5000;
